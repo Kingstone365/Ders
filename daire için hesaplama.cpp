9b3d4564f6054922e5eaf0eba19912b6a2d30a5a
Kingstone365 <yduzgun365@gmail.com>
2024-11-09 19:41:31 +0300

    Add files via upload

---
+++ daire için hesaplama.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cfloat>
+using namespace std;
+
+const float PI = 3.14f;
+int main()
+{
+	cout << "Please enter the raidus of circle:";
+	float raidus;
+	cin >> raidus;
+	float diameter = 2 * raidus;
+	float circumference = 2 * PI * raidus;
+	float areaofcircle = PI * raidus * raidus;
+	cout << "The diameter of the circle:" << diameter << endl;
+	cout << "The circumference of the circle:" << circumference << endl;
+	cout << "The area of the circle:" << areaofcircle << endl;
+}
